d08ec978ea03907bd056dfbfd9e37c0c88fae5f4
Sonia <adonisbudo@gmail.com>
2025-03-10 20:56:31 -0400

    Code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        import java.util.Scanner;
+
+
+        public class PasswordValidator {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter your password: ");
+                String password = scanner.nextLine();
+
+
+                if (isValidPassword(password)) {
+                    System.out.println("Password is strong.");
+                } else {
+                    System.out.println("Password does not meet the required conditions.");
+                }
+
+
+                scanner.close();
+            }
+
+
+            public static boolean isValidPassword(String password) {
+                if (password.length() < 8) {
+                    return false; // Must be at least 8 characters
+                }
+
+
+                boolean hasDigit = false;
+                boolean hasSpecialChar = false;
+                String specialCharacters = "!@#$%^&*()_+-=[]{};:'\"\\|,.<>?/";
+
+
+                for (char ch : password.toCharArray()) {
+                    if (Character.isDigit(ch)) {
+                        hasDigit = true;
+                    }
+                    if (specialCharacters.indexOf(ch) != -1) {
+                        hasSpecialChar = true;
+                    }
+                    if (hasDigit && hasSpecialChar) {
+                        return true; // If both conditions are met, no need to check further
+                    }
+                }
+
+
+                return false;
+            }
         }
+
+
+
     }
 }
